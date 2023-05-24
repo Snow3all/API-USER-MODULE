50e23a1a586bfea6f56ddde0845e46d600e3c893
snow3all <Netithorn_rly@hotmail.com>
2023-05-24 13:32:35 +0700

    [DONE] init project

---
+++ src/app.controller.ts
@@ -0,0 +1,8 @@
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+}
